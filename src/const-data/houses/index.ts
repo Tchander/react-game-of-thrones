86d01664bce8755ae68ac90f58855f860cc0e6d7
Tchander <evgeniy.tchander@yandex.ru>
2022-10-07 16:34:17 +0300

    Added all houses to enum

--- src/const-data/houses/index.ts
+++ src/const-data/houses/index.ts
@@ -4,7 +4,7 @@ import {IHouses} from "../../types";
 export const HOUSES: IHouses[] = [
     {
         name: HOUSES_NAMES.ARRYN,
-        title: 'Arryn',
+        title: 'placeholder',
         desktop: {
             top: '908px',
             left: '774px',
@@ -12,7 +12,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.BAELISH,
-        title: 'Baelish',
+        title: 'placeholder',
         desktop: {
             top: '1040px',
             left: '670px',
@@ -20,7 +20,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.BARATHEON,
-        title: 'Baratheon',
+        title: 'placeholder',
         desktop: {
             top: '1325px',
             left: '825px',
@@ -28,7 +28,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.BOLTON,
-        title: 'Bolton',
+        title: 'placeholder',
         desktop: {
             top: '430px',
             left: '750px',
@@ -36,7 +36,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.CLEGANE,
-        title: 'Clegane',
+        title: 'placeholder',
         desktop: {
             top: '1195px',
             left: '435px',
@@ -44,7 +44,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.FREY,
-        title: 'Frey',
+        title: 'placeholder',
         desktop: {
             top: '830px',
             left: '570px',
@@ -52,7 +52,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.GREYJOY,
-        title: 'Greyjoy',
+        title: 'placeholder',
         desktop: {
             top: '945px',
             left: '370px',
@@ -60,7 +60,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.KARSTARK,
-        title: 'Karstark',
+        title: 'placeholder',
         desktop: {
             top: '370px',
             left: '890px',
@@ -68,7 +68,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.LANNISTER,
-        title: 'Lannister',
+        title: 'placeholder',
         desktop: {
             top: '1100px',
             left: '360px',
@@ -76,7 +76,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.MARTELL,
-        title: 'Martell',
+        title: 'placeholder',
         desktop: {
             top: '1580px',
             left: '910px',
@@ -84,7 +84,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.MORMONT,
-        title: 'Mormont',
+        title: 'placeholder',
         desktop: {
             top: '285px',
             left: '475px',
@@ -92,7 +92,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.STARK,
-        title: 'Stark',
+        title: 'placeholder',
         desktop: {
             top: '480px',
             left: '620px',
@@ -100,7 +100,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.TARGARYEN,
-        title: 'Targaryen',
+        title: 'placeholder',
         desktop: {
             top: '1175px',
             left: '755px',
@@ -108,7 +108,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.TARLY,
-        title: 'Tarly',
+        title: 'placeholder',
         desktop: {
             top: '1475px',
             left: '505px',
@@ -116,7 +116,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.TARTH,
-        title: 'Tarth',
+        title: 'placeholder',
         desktop: {
             top: '1290px',
             left: '940px',
@@ -124,7 +124,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.TULLY,
-        title: 'Tully',
+        title: 'placeholder',
         desktop: {
             top: '990px',
             left: '560px',
@@ -132,7 +132,7 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.TYRELL,
-        title: 'Tyrell',
+        title: 'placeholder',
         desktop: {
             top: '1360px',
             left: '475px',
@@ -140,10 +140,155 @@ export const HOUSES: IHouses[] = [
     },
     {
         name: HOUSES_NAMES.UMBER,
-        title: 'Umber',
+        title: 'placeholder',
         desktop: {
             top: '310px',
             left: '770px',
         },
     },
+
+    // {
+    //     name: HOUSES_NAMES.ARRYN,
+    //     title: 'Arryn',
+    //     desktop: {
+    //         top: '908px',
+    //         left: '774px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.BAELISH,
+    //     title: 'Baelish',
+    //     desktop: {
+    //         top: '1040px',
+    //         left: '670px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.BARATHEON,
+    //     title: 'Baratheon',
+    //     desktop: {
+    //         top: '1325px',
+    //         left: '825px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.BOLTON,
+    //     title: 'Bolton',
+    //     desktop: {
+    //         top: '430px',
+    //         left: '750px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.CLEGANE,
+    //     title: 'Clegane',
+    //     desktop: {
+    //         top: '1195px',
+    //         left: '435px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.FREY,
+    //     title: 'Frey',
+    //     desktop: {
+    //         top: '830px',
+    //         left: '570px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.GREYJOY,
+    //     title: 'Greyjoy',
+    //     desktop: {
+    //         top: '945px',
+    //         left: '370px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.KARSTARK,
+    //     title: 'Karstark',
+    //     desktop: {
+    //         top: '370px',
+    //         left: '890px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.LANNISTER,
+    //     title: 'Lannister',
+    //     desktop: {
+    //         top: '1100px',
+    //         left: '360px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.MARTELL,
+    //     title: 'Martell',
+    //     desktop: {
+    //         top: '1580px',
+    //         left: '910px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.MORMONT,
+    //     title: 'Mormont',
+    //     desktop: {
+    //         top: '285px',
+    //         left: '475px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.STARK,
+    //     title: 'Stark',
+    //     desktop: {
+    //         top: '480px',
+    //         left: '620px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.TARGARYEN,
+    //     title: 'Targaryen',
+    //     desktop: {
+    //         top: '1175px',
+    //         left: '755px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.TARLY,
+    //     title: 'Tarly',
+    //     desktop: {
+    //         top: '1475px',
+    //         left: '505px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.TARTH,
+    //     title: 'Tarth',
+    //     desktop: {
+    //         top: '1290px',
+    //         left: '940px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.TULLY,
+    //     title: 'Tully',
+    //     desktop: {
+    //         top: '990px',
+    //         left: '560px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.TYRELL,
+    //     title: 'Tyrell',
+    //     desktop: {
+    //         top: '1360px',
+    //         left: '475px',
+    //     },
+    // },
+    // {
+    //     name: HOUSES_NAMES.UMBER,
+    //     title: 'Umber',
+    //     desktop: {
+    //         top: '310px',
+    //         left: '770px',
+    //     },
+    // },
 ];
